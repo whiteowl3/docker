7124c2899cdeeccb0be2194bcdebc185d32eedd1
whiteowl3 <71030468+whiteowl3@users.noreply.github.com>
2021-12-01 01:04:16 -0500

    Update entrypoint.sh
    "Lock" with rules.
    breaks support for multiple remote directives.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+ip route add 192.168/16 dev eth0 metric 5 table 100
+ip route add 172.16/12 dev eth0 metric 5 table 100
+ip route add prohibit default metric 10 table 100
+ip route add blackhole default metric 11 table 100
+
+REMOTE_ADDR=$(cat /config.ovpn | grep "remote " | cut -d " " -f 2)
+REMOTE_PORT=$(cat /config.ovpn | grep "remote " | cut -d " " -f 3)
+
+ip rule add not to $REMOTE_ADDR dport $REMOTE_PORT  table 100
+
+
 rm /etc/airvpn/hummingbird.lock
 /usr/bin/hummingbird --recover-network &> /dev/null
 
 touch ./airvpn_log
-/usr/bin/hummingbird -p tcp --persist-tun --network-lock off /config.ovpn &> airvpn_log &
+/usr/bin/hummingbird --persist-tun --network-lock off /config.ovpn &> airvpn_log &
 
 sleep 3
 
+ip route add default dev tun0 metric 5 table 100
 echo "nameserver $(grep -oP "(?<=\[DNS\]\s\[)[\d\.]*" airvpn_log)" > /etc/resolv.conf
 echo "nameserver $(grep -oP "(?<=\[DNS6\]\s\[)[a-zA-Z\d\:]*" airvpn_log)" >> /etc/resolv.conf
 
 tail -f -n 1000 airvpn_log
+
