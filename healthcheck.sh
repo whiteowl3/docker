a23c0203b87c0b656690582a19308812698cb735
whiteowl3 <71030468+whiteowl3@users.noreply.github.com>
2021-12-01 01:07:44 -0500

    Restart on unhealthy via kill 1

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,2 +1,4 @@
 #This finds the internal DNS IP and attempts to ping it.
-ping -i 10 -c 1 $(grep -Eo "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" /etc/resolv.conf)
+ping -i 10 -c 1 $(grep -Eo "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" /etc/resolv.conf) || bash -c 'kill -s 9 -1'
+# The following is probably a more sane way to do it, if we aren't in a hurry to restart
+# bash -c 'kill -s 15 -1 && (sleep 10; kill -s 9 -1)'
